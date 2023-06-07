8cfe2ca415e55415afdd0f67f8e29837eedabfd5
wilso3rc <wilso3rc@mail.uc.edu>
2023-06-06 20:29:48 -0400

    Variable declarations and output messages

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,64 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        int intOperandA = 2;
+        int intOperandB = 5;
+        int intSum = 8;
+        int intProduct = 12;
+        int intDifference = 8;
+        int intQuotient = 7;
+        int intModulo = 19;
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        double doubleOperandA = 2.50;
+        double doubleOperandB = 5.50;
+        double doubleSum = 8.70;
+        double doubleProduct = 12.90;
+        double doubleDifference = 8.40;
+        double doubleQuotient = 7.80;
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        intSum = intOperandA + intOperandB;
+        System.out.println("The sum using ints of " + intOperandA + " & " + intOperandB + " is " + intSum);
+
+        intProduct = intOperandA * intOperandB;
+        System.out.println("The product of the ints " + intOperandA + " & " + intOperandB + " is " + intProduct);
+
+        intDifference = intOperandA - intOperandB;
+        System.out.println("The difference off the ints " + intOperandA + " & " + intOperandB + " is " + intDifference);
+
+        intQuotient = intOperandB / intOperandA;
+        System.out.println("The Quotient of the ints " + intOperandB + " & " + intOperandA + " is " + intQuotient);
+
+        intModulo = intOperandB % intOperandA;
+        System.out.println("The Modulo of the ints " + intOperandB + " & " + intOperandA + " is " + intModulo);
+
+        doubleSum = doubleOperandA + doubleOperandB;
+        System.out.println("The sum using doubles of " + doubleOperandA + " & " + doubleOperandB + " is " + doubleSum);
+
+        doubleProduct = doubleOperandA * doubleOperandB;
+        System.out.println("The product of the doubles " + doubleOperandA + " & " + doubleOperandB + " is " + doubleProduct);
+
+        doubleDifference = doubleOperandA - doubleOperandB;
+        System.out.println("The difference of the doubles " + doubleOperandA + " & " + doubleOperandB + " is " + doubleDifference);
+
+        doubleQuotient = doubleOperandA / doubleOperandB;
+        System.out.println("The Quotient of the doubles " + doubleOperandA + " & " + doubleOperandB + " is " + doubleQuotient);
+
+        int kidsInFamily = 3;
+        String isItRaining = "no";
+        double gallonGasPrice = 3.65;
+        int favNumber = 11;
+        int shoeSize = 8;
+        String birthMonth = "July";
+        String fullName = "Rachael Wilson";
+        System.out.println("The number of kids in my family is: " + kidsInFamily);
+        System.out.println("The answer to is it raining is: " + isItRaining);
+        System.out.println("The price of a gallon of gas is: " + gallonGasPrice);
+        System.out.println("My favorite number is: " + favNumber);
+        System.out.println("My shoe size is: " + shoeSize);
+        System.out.println("My birth month is: " + birthMonth);
+        System.out.println("My full name is: " + fullName);
+
+
+
     }
 }
